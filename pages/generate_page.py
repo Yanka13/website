1fce3a1cfac866a8254c3a2ed34aa2020702a6e8
YiWen Hon <yiwench@gmail.com>
2022-03-09 15:44:40 +0000

    removed matplotlib from imports

--- pages/generate_page.py
+++ pages/generate_page.py
@@ -2,7 +2,6 @@ from email.utils import collapse_rfc2231_value
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 import tensorflow as tf
 from .utils import arr2PIL
@@ -26,48 +25,48 @@ def app():
         generated_images = model.predict(np.random.normal(0, 1, (32,100)))
         # fig, ax = plt.subplots(figsize=(1, 1))
         # im = ax.imshow(generated_images[0])col1, col2 = st.columns(2)
-        
+
         col1,col2,col3,col4=st.columns(4)
         with col1:
             col1.image(arr2PIL(generated_images[0]),use_column_width=True)
 
         with col2:
             col2.image(arr2PIL(generated_images[1]),use_column_width=True)
-            
+
         with col3:
             col3.image(arr2PIL(generated_images[2]),use_column_width=True)
-            
+
         with col4:
             col4.image(arr2PIL(generated_images[3]),use_column_width=True)
-            
+
         with col1:
             col1.image(arr2PIL(generated_images[4]),use_column_width=True)
 
         with col2:
             col2.image(arr2PIL(generated_images[5]),use_column_width=True)
-            
+
         with col3:
             col3.image(arr2PIL(generated_images[6]),use_column_width=True)
-            
+
         with col4:
             col4.image(arr2PIL(generated_images[7]),use_column_width=True)
-            
+
         with col1:
             col1.image(arr2PIL(generated_images[8]),use_column_width=True)
 
         with col2:
             col2.image(arr2PIL(generated_images[9]),use_column_width=True)
-            
+
         with col3:
             col3.image(arr2PIL(generated_images[10]),use_column_width=True)
-            
+
         with col4:
             col4.image(arr2PIL(generated_images[11]),use_column_width=True)
-            
-    
-        
+
+
+
         #col2.image(arr2PIL(final_image[0]),use_column_width=True)
-        
+
         # fig, axs = plt.subplots(4, 4, figsize=(11, 11))
         # axs[0, 0].imshow(generated_images[0])
         # axs[0, 1].imshow(generated_images[1])
@@ -87,15 +86,8 @@ def app():
         # axs[3, 3].imshow(generated_images[15])
         # #axs.set_title('plots from model')
         # st.pyplot(fig)
-    
+
         #st.write('I was clicked 🎉')
         #st.write('Further clicks are not visible but are executed')
     else:
         st.write('I was not clicked 😞')
-
-  
-  
-        
-        
-        
-        
