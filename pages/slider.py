47103cf10ae3b129903f64f8aef3f415b06c8033
Eloho Kemi <eloho_kemi@yahoo.co.uk>
2022-03-09 17:16:24 +0000

    updated with container and knn code, has errors

--- pages/slider.py
+++ pages/slider.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from .utils import arr2PIL
-
+from .find_k_neighbours import find_k_neighbours
+import skimage
 
 # def arr2PIL(arr):
 #     arr = (arr+1)/2*255
@@ -63,6 +64,53 @@ def app():
         slider_model = models[sliders]
         slider_image = slider_model.predict(noise)
         col1.image(arr2PIL(slider_image[0]),use_column_width=True)
+        
+        
+        with st.container():
+            st.write("This is inside the container")
+            
+            st.title('Find similar images')
+            st.write('Encodes images into latent space and finds 4 closest neighbours in that space')
+
+            st.set_option('deprecation.showfileUploaderEncoding', False)
+
+            #uploaded_file = st.file_uploader("Choose an image")
+
+            #if uploaded_file is not None:
+                #st.write("Your image:")
+            knn_image = (arr2PIL(slider_image[0]))
+            #with Image.open(arr2PIL(slider_image[0])) as img:
+            resized_img = knn_image.skimage.transform.resize((128, 128))
+            st.image(resized_img)
+            st.write("Similar images:")
+
+            # find_k_neighbours
+            result = find_k_neighbours(image = resized_img, file_location = "data/wikiart_scraped.csv")
+
+            col1, col2, col3, col4 = st.columns(4)
+
+            with col1:
+                image1 = skimage.io.imread(f"images/abstract_ex/{result[0]['Image_filename']}", as_gray=False)
+                st.image(image1)
+                st.write(f"{result[0]['Title']}" )
+                st.write(f"by {result[0]['Artist']}")
+
+            with col2:
+                st.image(f"images/abstract_ex/{result[1]['Image_filename']}")
+                st.write(f"{result[1]['Title']}")
+                st.write(f"by {result[1]['Artist']}")
+
+            with col3:
+                st.image(f"images/abstract_ex/{result[2]['Image_filename']}")
+                st.write(f"{result[2]['Title']}")
+                st.write(f"by {result[2]['Artist']}")
+
+
+            with col4:
+                st.image(f"raw_data/abstract_ex/{result[3]['Image_filename']}")
+                st.write(f"{result[3]['Title']}")
+                st.write(f"by {result[3]['Artist']}")
+
 
 #st.slider('MODEL SELECTION',0,len(model_list)-1,0,step=1, key=st.session_state['slider'])
 #models = load_models(list(model_list.values()))
